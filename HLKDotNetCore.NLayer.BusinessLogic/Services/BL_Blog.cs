bbeee307b8b8a50c2354c33cb9a93395193479a6
William lynn <13thwerewolf@gmail.com>
2024-05-31 11:59:09 +0630

    Complete [Nlayer]

---
+++ HLKDotNetCore.NLayer.BusinessLogic/Services/BL_Blog.cs
@@ -0,0 +1,44 @@
+﻿using HLKDotNetCore.NLayer.DataAccess.Services;
+using HLKDotNetCore.NLayer.DataAccess.Models;
+
+namespace HLKDotNetCore.RestApiWithNLayer.Features.Blog
+{
+    public class BL_Blog
+    {
+        private readonly DA_Blog _daBlog;
+        public BL_Blog()
+        {
+            _daBlog = new DA_Blog();
+        }
+
+        public List<BlogModel> GetBlogs()
+        {
+            return _daBlog.GetBlogs();
+        }
+
+        public BlogModel GetBlog(int id)
+        {
+            return _daBlog.GetBlog(id);
+        }
+
+        public int DeleteBlog(int id)
+        {
+            return _daBlog.DeleteBlog(id);
+        }
+
+        public int CreateBlog(BlogModel reqModel)
+        {
+            return _daBlog.CreateBlog(reqModel);
+        }
+
+        public int UpdateBlog(int id, BlogModel reqModel)
+        {
+            return _daBlog.UpdateBlog(id, reqModel);
+        }
+
+        public int PatchBlog(int id, BlogModel reqModel)
+        {
+            return _daBlog.PatchBlog(id, reqModel);
+        }
+    }
+}
---
+++ HLKDotNetCore.NLayer.DataAccess/DataBase/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using HLKDotNetCore.NLayer.DataAccess.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HLKDotNetCore.NLayer.DataAccess.DataBase
+{
+    internal class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(ConnectionStrings.SqlConnectionStringBuilder.ConnectionString);
+        }
+        public DbSet<BlogModel> Blogs { get; set; }
+    }
+}
